2f3efb6aee7a04099de57a8d4bc1cfefefc54834
gerlindekihn <gerlinde.kihn@blueml-online.de>
2022-12-03 14:57:48 +0100

    Initial commit

---
+++ AoC2022/Day01/Day01.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC2022.Day01;
+
+internal class Day01
+{
+    string[] caloriesInput = File.ReadAllLines("Day01/Input");
+    List<int> caloriesPerElf = new List<int>();
+
+    public Day01()
+    {
+        int currentElfCalories = 0;
+
+        foreach (string line in caloriesInput)
+        {
+            if (line != string.Empty)
+            {
+                int currentCalories = Convert.ToInt32(line);
+                currentElfCalories = currentElfCalories + currentCalories;
+            }
+            else
+            {
+                caloriesPerElf.Add(currentElfCalories);
+                currentElfCalories = 0;
+            }
+        }
+    }
+
+    public int SolvePart1()
+    {
+        return caloriesPerElf.Max();
+    }
+    
+    public int SolvePart2()
+    {
+        int totalCaloriesOfTopThreeElves = caloriesPerElf.OrderDescending().Take(3).Sum();
+        return totalCaloriesOfTopThreeElves;
+    }
+
+    
+}
---
+++ AoC2022/Program.cs
@@ -0,0 +1,6 @@
+﻿using AoC2022.Day01;
+
+Day01 Day01 = new();
+Console.WriteLine("Solve day 1:");
+Console.WriteLine(Day01.SolvePart1());
+Console.WriteLine(Day01.SolvePart2());
