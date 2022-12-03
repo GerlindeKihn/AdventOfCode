1760187bcf7fe37c9f842ad6c5df240a50ac7c2d
gerlindekihn <gerlinde.kihn@blueml-online.de>
2022-12-03 15:03:16 +0100

    Day Day02

---
+++ AoC2022/Day02/Day02.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC2022.Day02
+{
+    internal class Day02
+    {
+        const int ROCK = 1;
+        const int PAPER = 2;
+        const int SCISSORS = 3;
+
+        int myScore = 0;
+
+        string[] rockPaperScissorsInputFile = File.ReadAllLines("Day02/Input");
+
+        public Day02()
+        {
+            char opponentRock = 'A';
+            char opponentPaper = 'B';
+            char opponentScissors = 'C';
+
+            char myRock = 'X';
+            char myPaper = 'Y';
+            char myScissors = 'Z';
+
+            foreach (string line in rockPaperScissorsInputFile)
+            {
+                char opponent = line[0];
+                char me = line[2];
+
+                int playerOpponent = 0;
+                int playerMe = 0;
+
+                if (opponent == opponentRock)
+                {
+                    playerOpponent = ROCK;
+                }
+                else if (opponent == opponentPaper)
+                {
+                    playerOpponent = PAPER;
+                }
+                else if (opponent == opponentScissors)
+                {
+                    playerOpponent = SCISSORS;
+                }
+                else
+                {
+                    Console.WriteLine("Oops, there must be a mistake!");
+                }
+
+                if (me == myRock)
+                {
+                    playerMe = ROCK;
+                }
+                else if (me == myPaper)
+                {
+                    playerMe = PAPER;
+                }
+                else if (me == myScissors)
+                {
+                    playerMe = SCISSORS;
+                } 
+                else
+                {
+                    Console.WriteLine("Oops, there must be a mistake!");
+                }
+
+                myScore += playerMe;
+
+                if (playerOpponent == playerMe)
+                {
+                    myScore += 3;
+                }
+                else if (playerMe == ROCK && playerOpponent == SCISSORS)
+                {
+                    myScore += 6;
+                }
+                else if (playerMe == PAPER && playerOpponent == ROCK)
+                {
+                    myScore += 6;
+                }
+                else if (playerMe == SCISSORS && playerOpponent == PAPER)
+                {
+                    myScore += 6;
+                }
+            }
+        }
+
+        public int SolvePart1()
+        {
+            return myScore;
+        }
+
+        //public int SolvePart2()
+        //{
+        //    return 1;
+        //}
+    }
+}
--- AoC2022/Program.cs
+++ AoC2022/Program.cs
@@ -1,6 +1,11 @@
 ﻿using AoC2022.Day01;
+using AoC2022.Day02;
 
 Day01 Day01 = new();
 Console.WriteLine("Solve day 1:");
 Console.WriteLine(Day01.SolvePart1());
 Console.WriteLine(Day01.SolvePart2());
+
+Day02 Day02 = new Day02();
+Console.WriteLine("Solve day 2:");
+Console.WriteLine(Day02.SolvePart1());
